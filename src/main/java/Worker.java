91265c3d4211f37452fdad16c48932e9b8515df9
Vivek Pandey <viv.793@gmail.com>
2020-11-03 00:36:13 +0800

    Adding worker

--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -1,6 +1,4 @@
-
 import org.apache.zookeeper.*;
-import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,84 +6,105 @@ import java.io.IOException;
 import java.util.Random;
 
 
-public class Worker implements Watcher
-{
+public class Worker implements Watcher {
     private static final Logger LOG = LoggerFactory.getLogger(Worker.class);
-    private ZooKeeper zooKeeper;
-    private String connectionString;
     private final Random random = new Random();
-    String serverId = Integer.toHexString(random.nextInt());
-    private static final int SESSION_TIMEOUT_IN_MS = 15000;
-    private String status;
+    private final int SESSION_TIMEOUT = 15000;
+    private final String connectionString;
+    private final String serverId = Integer.toHexString(random.nextInt());
+    private ZooKeeper zooKeeper;
+    private volatile boolean expired;
+    private volatile boolean connected;
 
     public Worker(String connectionString) {
         this.connectionString = connectionString;
     }
 
-    public void process(WatchedEvent watchedEvent) {
-        LOG.info(watchedEvent.toString());
+    void startZk() throws IOException {
+        this.zooKeeper = new ZooKeeper(this.connectionString, SESSION_TIMEOUT, this);
     }
 
-    private void startZk() throws IOException {
-        this.zooKeeper = new ZooKeeper(this.connectionString, SESSION_TIMEOUT_IN_MS, this);
-    }
-
-    private void stopZk() throws InterruptedException {
+    void stopZk() throws InterruptedException {
         this.zooKeeper.close();
     }
 
-    private void register() {
-        zooKeeper.create("/workers/worker-" + this.serverId,
-                "Idle".getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL,
-                createWorkerCallback,
-                null);
+    public void process(WatchedEvent watchedEvent) {
+        if (watchedEvent.getType() == Event.EventType.None) {
+            switch (watchedEvent.getState()) {
+                case SyncConnected:
+                    connected = true;
+                    expired = false;
+                    break;
+                case Expired:
+                    expired = true;
+                    connected = false;
+                    LOG.error("Session expired");
+                    break;
+                case Disconnected:
+                    connected = false;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
-    private AsyncCallback.StringCallback createWorkerCallback = new AsyncCallback.StringCallback() {
-        public void processResult(int i, String s, Object o, String s1) {
-            switch (KeeperException.Code.get(i)) {
+    public boolean isConnected() {
+        return this.connected;
+    }
+
+    public boolean isExpired() {
+        return this.expired;
+    }
+
+    AsyncCallback.StringCallback createWorkerCallback = new AsyncCallback.StringCallback() {
+        public void processResult(int rc, String path, Object ctx, String name) {
+            switch (KeeperException.Code.get(rc)) {
                 case OK:
-                    LOG.info("Registered successfully " + serverId);
+                    LOG.info("Worker registered successfully.");
                     break;
                 case NODEEXISTS:
-                    LOG.warn("Node already registered " + serverId);
+                    LOG.warn("Worker already created.");
                     break;
                 case CONNECTIONLOSS:
+                    LOG.info("Connection lost. Trying to create again.");
                     register();
-                    return;
+                    break;
                 default:
-                    LOG.error("Something went wrong: " + KeeperException.create(KeeperException.Code.get(i), s));
+                    LOG.error("Could not register worker", KeeperException.create(rc, path));
             }
         }
     };
 
-    private AsyncCallback.StatCallback statusUpdateCallback = new AsyncCallback.StatCallback() {
-        public void processResult(int i, String s, Object o, Stat stat) {
-            switch (KeeperException.Code.get(i)) {
+    public void register() {
+        this.zooKeeper.create("/workers/" + serverId, "Idle".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, createWorkerCallback, null);
+    }
+
+    public void bootstrap() {
+        this.createAssignNode();
+    }
+
+    private void createAssignNode() {
+        this.zooKeeper.create("/assign/worker-" + serverId, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT, createAssignNodeCallback, null);
+    }
+
+    private AsyncCallback.StringCallback createAssignNodeCallback = new AsyncCallback.StringCallback() {
+        public void processResult(int rc, String path, Object ctx, String name) {
+            switch (KeeperException.Code.get(rc)) {
+                case OK:
+                    LOG.info("Assign node created");
+                    break;
+                case NODEEXISTS:
+                    LOG.warn("Assign node already exists");
+                    break;
                 case CONNECTIONLOSS:
-                    updateStatus((String)o);
-                    return;
+                    LOG.warn("Assign node failed due to connection loss. Trying again.");
+                    createAssignNode();
+                    break;
+                default:
+                    LOG.error("Unknown error while creating assing node", KeeperException.create(rc, path));
             }
         }
-    };
-
-    synchronized private void updateStatus(String status) {
-        if (status == this.status) {
-            this.zooKeeper.setData("/workers" + serverId, status.getBytes(), -1, statusUpdateCallback, status);
-        }
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Worker w = new Worker(args[0]);
-        w.startZk();
-        w.register();
-        Thread.sleep(100000);
     }
 
 }
